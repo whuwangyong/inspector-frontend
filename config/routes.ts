9a6c214e49e777688af25e95b5c6a9e1c659fd44
whuwangyong <whuwangyong@gmail.com>
2023-08-11 10:30:44 +0800

    feat: 删除登陆相关逻辑，但是还剩下一个头像图标不知道如何去除

--- config/routes.ts
+++ config/routes.ts
@@ -1,15 +1,10 @@
 export default [
-  {
-    path: '/user',
-    layout: false,
-    routes: [{ name: '登录', path: '/user/login', component: './User/Login' }],
-  },
   { path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome' },
   {
     path: '/admin',
     name: '管理页',
     icon: 'crown',
-    access: 'canAdmin',
+    // access: 'canAdmin',
     routes: [
       { path: '/admin', redirect: '/admin/sub-page' },
       { path: '/admin/sub-page', name: '二级管理页', component: './Admin' },
